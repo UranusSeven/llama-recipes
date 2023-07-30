11554d52f2f4dc0eeb2c6c1ba49e91e23b3671d5
UranusSeven <109661872+UranusSeven@users.noreply.github.com>
2023-07-30 15:57:04 +0800

    ENH: fine-tune llama-2-chat

--- ft_datasets/__init__.py
+++ ft_datasets/__init__.py
@@ -2,5 +2,5 @@
 # This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
 
 from .grammar_dataset import get_dataset as get_grammar_dataset
-from .alpaca_dataset import InstructionDataset as get_alpaca_dataset
+from .alpaca_dataset_llama2_chat import InstructionDataset as get_alpaca_dataset
 from .samsum_dataset import get_preprocessed_samsum as get_samsum_dataset
---
+++ ft_datasets/alpaca_dataset_llama2_chat.py
@@ -0,0 +1,86 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
+
+# For dataset details visit: https://crfm.stanford.edu/2023/03/13/alpaca.html
+
+import copy
+import json
+import os
+import torch
+
+from sentencepiece import SentencePieceProcessor
+from torch.utils.data import Dataset
+from typing import List
+
+prefix = (
+    "<<SYS>>\nYou are a helpful, respectful and honest assistant. "
+    "Always answer as helpfully as possible, while being safe.  "
+    "Your answers should not include any harmful, unethical, "
+    "racist, sexist, toxic, dangerous, or illegal content. "
+    "Please ensure that your responses are socially unbiased and positive in nature.\n"
+    "If a question does not make any sense, or is not factually coherent, "
+    "explain why instead of answering something not correct. "
+    "If you don't know the answer to a question, please don't share false information.\n<</SYS>>\n\n"
+)
+
+PROMPT_DICT = {
+    "prompt_input": (
+        "<s>[INST] " + prefix +
+        "{instruction}\nInput:{input} [/INST]"
+    ),
+    "prompt_no_input": (
+        "<s>[INST] " + prefix +
+        "{instruction} [/INST]"
+    ),
+}
+
+class InstructionDataset(Dataset):
+    def __init__(self, dataset_config, tokenizer, partition="train", max_words=30):
+        self.ann = json.load(open(dataset_config.data_path))
+        if partition == "train":
+            self.ann = self.ann
+        else:
+            self.ann = self.ann[:200]
+
+        self.max_words = max_words
+        # tokenizer = Tokenizer(model_path=model_path + "./tokenizer.model")
+        self.tokenizer = tokenizer
+        # self.tokenizer1 = tokenizer
+
+    def __len__(self):
+        return len(self.ann)
+
+    def __getitem__(self, index):
+        ann = self.ann[index]
+        if ann.get("input", "") == "":
+            prompt = PROMPT_DICT["prompt_no_input"].format_map(ann)
+        else:
+            prompt = PROMPT_DICT["prompt_input"].format_map(ann)
+        example = prompt + ann["output"]
+        prompt = torch.tensor(
+            self.tokenizer.encode(prompt), dtype=torch.int64
+        )
+        example = self.tokenizer.encode(example)
+        example.append(self.tokenizer.eos_token_id)
+        example = torch.tensor(
+            example, dtype=torch.int64
+        )
+        padding = self.max_words - example.shape[0]
+        if padding > 0:
+            example = torch.cat((example, torch.zeros(padding, dtype=torch.int64) - 1))
+        elif padding < 0:
+            example = example[: self.max_words]
+        labels = copy.deepcopy(example)
+        labels[: len(prompt)] = -1
+        example_mask = example.ge(0)
+        label_mask = labels.ge(0)
+        example[~example_mask] = 0
+        labels[~label_mask] = 0
+        example_mask = example_mask.float()
+        label_mask = label_mask.float()
+
+        return {
+            "input_ids": example,
+            "labels": labels,
+            "attention_mask":example_mask,
+        }
